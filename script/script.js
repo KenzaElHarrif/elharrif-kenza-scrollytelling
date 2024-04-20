37fed7540a208a88b49ee828057b697c9323db46
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2024-04-20 16:35:07 -0400

    ajout du texte + essaie de défilement de page gsap

--- script/script.js
+++ script/script.js
@@ -14,3 +14,9 @@ window.addEventListener("scroll", () => {
 
 gsap.set('.appel', { y: 0, ease:"power3.in"})
 gsap.from('.appel', { y: '35%', repeat:-1, yoyo:true, ease:"power1.inOut"});
+
+/*------Défilement de chaque chapitres--------*/
+
+/*gsap.to('.defilement', { y: "-300vh", ease:"linear", duration:5})
+gsap.to('.scene1-lilo-triste-1', {y: "-300vh", ease:"linear"})
+gsap.to('.scene1-lilo-triste-2', { y: "-300vh", ease:"linear"})*/
