d6ae9e89e52099d3cb07cef34f647d8abdf3ded6
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2024-04-25 13:40:55 -0400

    animations sans scrolltrigger chapitre1 et chapitre2

--- script/script.js
+++ script/script.js
@@ -33,23 +33,57 @@ scrollTrigger: {
 
 //gsap.to(".scene1-lilo-triste-1, .scene1-lilo-triste-1", {y:"300vh"})
 
+//CHAPITRE 1
 gsap.timeline()
+//parallax
+.to(".scene1-parallax1", { backgroundPosition: "100% 0", ease: "none",duration:22, repeat: -1},0)
+.to(".scene1-parallax2", { backgroundPosition: "100% 0", ease: "none",duration:19, repeat: -1},0)
+.to(".scene1-parallax3", { backgroundPosition: "100% 0", ease: "none",duration:15, repeat: -1},0)
+.to(".scene1-parallax4", { backgroundPosition: "100% 0", ease: "none",duration:13, repeat: -1},0)
 //apparition titre
-.fromTo(".scene1-titre", {opacity:1}, {opacity:1, duration:2})
-.fromTo(".scene1-titre", {opacity:1}, {opacity:0, duration:4})
+.fromTo(".scene1-titre", {opacity:1}, {opacity:1, duration:2},0)
+.fromTo(".scene1-titre", {opacity:1}, {opacity:0, duration:4},3)
 //apparition scène 1 partie 1
-.fromTo(".scene1-parallax1, .scene1-parallax2, .scene1-parallax3, .scene1-parallax4", {opacity:1}, {opacity:1, duration:2})
-.fromTo(".scene1-parallax1, .scene1-parallax2, .scene1-parallax3, .scene1-parallax4", {opacity:1}, {opacity:0, duration:4})
+.fromTo(".scene1-parallax1, .scene1-parallax2, .scene1-parallax3, .scene1-parallax4", {opacity:1}, {opacity:1, duration:1})
+//note à moi-même: animations texte 1 et texte 2 entre entré et sortie parallax<-----
+.fromTo(".scene1-parallax1, .scene1-parallax2, .scene1-parallax3, .scene1-parallax4, .scene1-texte1, .scene1-texte2", {opacity:1}, {opacity:0, duration:4},)
+//note à moi-même: animations texte 3 et texte 4 entre entré et sortie parallax<-----
 .fromTo('.scene1-lilo-triste-1', {x:0, opacity:0},{x:"11vw",opacity:1, ease:"power1.out", duration:3.5,},0.5)
 .fromTo('.scene1-lilo-triste-1', {y:"54vh"},{y: "53vh", ease:"power1.inOut", yoyo:true, repeat:-1, duration:0.5},0.5)
-.fromTo('.scene1-lilo-triste-1', {opacity:1},{opacity:0, duration:5},8.5)
-//apparition scène 1 partie 2
-.fromTo('.scene1-lilo-triste-2', {opacity:0},{opacity:0, ease:"power2.inOut", duration:2.5,},0)
-.fromTo('.scene1-lilo-triste-2', {y:"54vh"},{y: "53vh", ease:"power1.inOut", yoyo:true, repeat:-1, duration:0.5,}, 14)
-.fromTo('.scene1-lilo-triste-2', {opacity:0},{opacity:1, ease:"power2.inOut", duration:2.5,}, 15)
-.fromTo('.scene1-lilo-triste-2', {x:0},{x:"69vw", ease:"power1.inOut", duration:8},15.5)
-.fromTo('.scene1-lilo-triste-2', {rotate:0},{rotate:25, ease:"power1.Out", duration:0.7,}, 22)
-.fromTo('.scene1-lilo-triste-2', {opacity:1},{opacity:0, ease:"power2.inOut", duration:0.5}, 22.3)
+.fromTo('.scene1-lilo-triste-1', {x:"11vw"},{x:"65vw", ease:"power1.inOut", duration:8},10.5)
+.fromTo('.scene1-lilo-triste-1', {rotate:0},{rotate:25, ease:"power1.Out", duration:0.7,}, 14.5)
+.fromTo('.scene1-lilo-triste-1', {opacity:1},{opacity:0, ease:"power2.inOut", duration:0.5}, 14.7)
+
+
+//CHAPITRE 2
+gsap.timeline()
+
+.fromTo(".scene2-lilo-tombe", {y:0, opacity:1},{y:"30vh", opacity:0, duration:3})
+.fromTo(".scene2-titre",{y:0}, {y:"-100vh", ease:"none", duration:3},2.5)
+.fromTo(".scene2",{y:"0"}, {y:"-100vh", ease:"none", duration:3},2.5)
+.fromTo(".sur-nuage-1",{y:"150vh"}, {y:"150vh", ease:"none", duration:3},0)
+.fromTo(".sur-nuage-1",{y:"150vh"}, {y:"-60vh", ease:"power1.inOut", duration:5},2.5)
+.fromTo('.sur-nuage-1', {y:"-60vh"},{y: "-61vh", ease:"power1.inOut", yoyo:true, repeat:-1, duration:0.5})
+
+//note à moi-même: texte va se mettre ici<--------------
+.fromTo(".sur-nuage-1",{y:"-61vh", opacity:1}, {y:"-200vh", ease:"power1.inOut", opacity:0, duration:4},10.5)
+.fromTo(".scene2",{}, {y:"-300vh", ease:"none", duration:5}, 10.5)
+.fromTo(".scene2-poisson1",{y:"-100vh"},{y:"-300vh",ease:"none", duration:5}, 10.5)
+.fromTo(".scene2-poisson1",{opacity:0, x:0},{x:"20vh", opacity:1, ease:"power1.Out", duration:2}, 14.8)
+.fromTo(".scene2-poisson2",{y:"-100vh"},{y:"-300vh",ease:"none", duration:5}, 10.5)
+.fromTo(".scene2-poisson2",{opacity:0, x:0},{x:"20vh", opacity:1, ease:"power1.Out", duration:2}, 14.5)
+.fromTo(".scene2-poisson3",{y:"-100vh"},{y:"-300vh",ease:"none", duration:5}, 10.5)
+.fromTo(".scene2-poisson3",{opacity:0, x:0},{x:"20vh", opacity:1, ease:"power1.Out", duration:2}, 15.2)
+.fromTo(".scene2-poisson4",{y:"-100vh"},{y:"-300vh",ease:"none", duration:5}, 10.5)
+.fromTo(".scene2-poisson4",{opacity:0, x:0},{x:"-20vh", opacity:1, ease:"power1.Out", duration:2}, 15.8)
+
+
+//CHAPITRE 3
+
+
+
+
+
  
     let h2 = document.querySelectorAll("h2");
     let h3 = document.querySelectorAll("h3");
@@ -57,8 +91,3 @@ gsap.timeline()
     gsap.timeline()
     .fromTo(h2, {opacity:0, y:0},{opacity:1, y: "-3vh", ease:"power1.inOut", duration:1})
     .fromTo(h3, {opacity:0, x:"-3vw"},{opacity:1, x: 0, ease:"power1.inOut", duration:1.5},0.5)
-
-    gsap.timeline()
-    gsap.to(".scene1-parallax1", {
-        backgroundPosition: "50% 100%",
-        ease: "none"})
